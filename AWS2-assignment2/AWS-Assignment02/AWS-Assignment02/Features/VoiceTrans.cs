b9181ec6d9870f22391ba39403ba35822ca14213
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-05-27 07:00:01 +0700

    done assignment 2

--- AWS2-assignment2/AWS-Assignment02/AWS-Assignment02/Features/VoiceTrans.cs
+++ AWS2-assignment2/AWS-Assignment02/AWS-Assignment02/Features/VoiceTrans.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Amazon.Polly;
+using Amazon.Polly.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace AWS_Assignment02.Features
 {
@@ -29,42 +33,107 @@ namespace AWS_Assignment02.Features
             txtEn.ScrollBars = ScrollBars.Both;
             txtEn.SelectionStart = txtEn.Text.Length;
             txtEn.ScrollToCaret();
-            txtEn.BackColor = Color.LightCyan;
+            txtEn.BackColor = System.Drawing.Color.LightCyan;
             txtEn.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            txtEn.Text = "Hello, I am Hòa. I'm from Vietnamese.";
             this.Controls.Add(txtEn);
 
             Label lbl = new Label();
             lbl.Text = "Chọn giọng đọc";
-            lbl.Location = new System.Drawing.Point(5, 380);
+            lbl.Location = new System.Drawing.Point(50, 380);
+            lbl.Size = new Size(120, 30);
             lbl.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            lbl.BackColor = Color.Blue;
-            lbl.ForeColor = Color.Red;
+            //lbl.BackColor = Color.Blue;
+            lbl.ForeColor = System.Drawing.Color.Red;
             this.Controls.Add(lbl);
 
+            cbVoice.Location = new System.Drawing.Point(200, 375);
+            cbVoice.Size = new Size(200, 40);
+            cbVoice.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            var voiceArr = new string[] { 
+                "Salli, Female", "Joanna, Female", "Ivy, Female", "Kendra, Female",
+                "Kimberly, Female", "Matthew, Male", "Justin, Male", "Joey, Male"
+            };
+            cbVoice.DataSource = voiceArr;
+            cbVoice.SelectedIndex = cbVoice.Items.IndexOf("Salli, Female");
+            this.Controls.Add(cbVoice);
+
             Button btnTrans = new Button();
-            btnTrans.Location = new System.Drawing.Point(500, 380);
+            btnTrans.Location = new System.Drawing.Point(450, 370);
             btnTrans.Text = "Nghe thử";
-            btnTrans.Size = new Size(80, 40);
+            btnTrans.Size = new Size(100, 35);
             btnTrans.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            btnTrans.BackColor = Color.Blue;
-            btnTrans.ForeColor = Color.White;
+            btnTrans.BackColor = System.Drawing.Color.Blue;
+            btnTrans.ForeColor = System.Drawing.Color.White;
             btnTrans.Click += BtnTrans_Click;
             this.Controls.Add(btnTrans);
-
-            
         }
+
+       
 
         private void BtnTrans_Click(object sender, EventArgs e)
         {
+            this.Text = "Đang tải";
+            var client = new AmazonPollyClient();
+
+            var request = new SynthesizeSpeechRequest
+            {
+                Text = txtEn.Text,
+                OutputFormat = OutputFormat.Mp3,
+                VoiceId = getVoiceId(),
+            };
+
+            var response = client.SynthesizeSpeech(request);
+
+            var currentFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var fileName = $"{currentFolder}{Guid.NewGuid()}.mp3";
+
+            using (var fileStream = File.Create(fileName))
+            {
+                response.AudioStream.CopyTo(fileStream);
+                fileStream.Flush();
+                fileStream.Close();
+            }    
+
             try
             {
-                //System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\nguyenhuuhoa\Desktop\Anh-Oi-O-Lai-Chi-Pu-Dat-G.mp3");
-                //player.Play();
+                var player = new MediaPlayer();
+                player.Open(new Uri(fileName, UriKind.Absolute));
+                player.Play();
+                player.MediaOpened += Player_MediaOpened;
+                player.MediaEnded += Player_MediaEnded;
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void Player_MediaOpened(object sender, EventArgs e)
+        {
+            this.Text = "Đang phát";
+        }
+
+        private void Player_MediaEnded(object sender, EventArgs e)
+        {
+            this.Text = "Chuyển văn bản thành giọng nói";
+        }
+
+        private VoiceId getVoiceId()
+        {
+            switch (cbVoice.SelectedIndex)
+            {
+                case 0: return VoiceId.Salli;
+                case 1: return VoiceId.Joanna;
+                case 2: return VoiceId.Ivy;
+                case 3: return VoiceId.Kendra;
+                case 4: return VoiceId.Kimberly;
+                case 5: return VoiceId.Matthew;
+                case 6: return VoiceId.Justin;
+                case 7: return VoiceId.Joey;
+            }
+            return VoiceId.Joanna;
         }
     }
 }
--- AWS2-assignment2/AWS-Assignment02/AWS-Assignment02/Program.cs
+++ AWS2-assignment2/AWS-Assignment02/AWS-Assignment02/Program.cs
@@ -17,9 +17,9 @@ namespace AWS_Assignment02
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Main());
+            Application.Run(new Main());
             //Application.Run(new Translate());
-            Application.Run(new VoiceTrans());
+            //Application.Run(new VoiceTrans());
         }
     }
 }
